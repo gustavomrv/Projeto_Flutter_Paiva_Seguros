791bad0771faee3b851fd95eab4e831ed51d3fa8
gustavomrv <gustavomourago@gmail.com>
2022-07-22 21:20:33 -0300

    drawer_add

--- paiva_seguros/lib/screen/home_cliente.dart
+++ paiva_seguros/lib/screen/home_cliente.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:paiva_seguros/screen/my_drawer.dart';
 
 class HomeClienteScreen extends StatelessWidget {
   HomeClienteScreen({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(   
-      appBar: AppBar(title: Text("Home Cliente"), backgroundColor: Colors.red,)
+      appBar: AppBar(title: Text("Home Cliente"), backgroundColor: Colors.red,),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ paiva_seguros/lib/screen/my_drawer.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+import 'package:paiva_seguros/screen/login.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
+        DrawerHeader(
+          child: Text(""),
+          decoration: BoxDecoration(color: Colors.red),
+        ),
+        SizedBox(height: 450,),
+        ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            bottomLeft: Radius.circular(32),
+            ),
+          child: ListTile(
+            leading: Icon(Icons.open_in_browser, color: Colors.red, size: 30,),
+            title: Text('Sair', style: TextStyle(color: Colors.red, fontSize: 18),),
+            onTap: () {
+              Navigator.of(context)
+                .push(MaterialPageRoute(builder: (v) => LoginScreen()));
+            },
+            selected: true,
+          ),
+        ),
+      ]),
+    );
+  }
+}
