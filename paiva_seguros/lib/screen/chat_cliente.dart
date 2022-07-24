e191476ecbd8e6a3139391f2755399ac9522902c
gustavomrv <gustavomourago@gmail.com>
2022-07-24 13:53:14 -0300

    back_chat_sucess

--- paiva_seguros/lib/screen/chat_cliente.dart
+++ paiva_seguros/lib/screen/chat_cliente.dart
@@ -8,6 +8,8 @@ import 'package:query/query.dart';
 import 'package:paiva_seguros/screen/login.dart';
 import 'package:paiva_seguros/screen/my_drawer.dart';
 import 'package:paiva_seguros/store/chat_cliente_store.dart';
+import 'package:backendless_sdk/backendless_sdk.dart';
+
 
 class ChatCliente extends StatelessWidget {
 
@@ -20,10 +22,27 @@ class ChatCliente extends StatelessWidget {
     dynamic email_map = ModalRoute.of(context)?.settings.arguments;
     String email = email_map["email"];
     
-    final mensagens_cliente = FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: email).where('destinatario', isEqualTo: "gustavomourago@gmail.com").orderBy('tempo');
-    final mensagens_corretor = FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: "gustavomourago@gmail.com").where('destinatario', isEqualTo: email).orderBy('tempo');
-    final mensagens_juntas = mensagens_cliente.mensagens_corretor;
+    //dynamic mensagens_cliente = FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: email).where('destinatario', isEqualTo: "gustavomourago@gmail.com").orderBy('tempo');
+    //dynamic mensagens_corretor = FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: "gustavomourago@gmail.com").where('destinatario', isEqualTo: email).orderBy('tempo');
+    
+    var nome_map = FirebaseFirestore.instance.collection("cliente").where('email', isEqualTo: email);
+    String nome = '';
 
+    StreamBuilder<QuerySnapshot>(
+      stream: nome_map.snapshots(),
+      builder: (context, snap) {
+        if (snap.hasData) {
+          List<DocumentSnapshot> documents = snap.data!.docs;
+          documents.map(
+            (e) => nome = e['nome'],
+          ).toList();
+        }
+        return Column();
+      }
+    );
+
+    var mensagens_juntas = FirebaseFirestore.instance.collection("mensagem").orderBy('tempo');
+    
     return Scaffold(   
       appBar: AppBar(title: Text("Conversa com Gustavo Moura"), backgroundColor: Colors.red,),
       drawer: MyDrawer(),
@@ -33,81 +52,113 @@ class ChatCliente extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: SingleChildScrollView(   
-        child: StreamBuilder<QuerySnapshot>(
-          // stream: FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: email).where('destinatario', isEqualTo: "gustavomourago@gmail.com").orderBy('tempo').snapshots(),
-          stream: mensagens_cliente.snapshots(),
-          builder: (context, snap) {
-            if (snap.hasData) {
-              List<DocumentSnapshot> documents = snap.data!.docs;
-              return Column(
-                children: [
-                  ListView(
-                    shrinkWrap: true,
-                    children:           
-                    documents.map(
-                      (e) => Column(
-                        children: [
-                          Card(
-                            child: Column(
-                              children: [
-                                ListTile(      
-                                  onTap: (){},  
-                                  hoverColor: Colors.black,              
-                                  title: Text(e['texto']),
-                                  subtitle: Text(e['remetente']),
-                                ),        
-                              ],
-                            ),
-                            color: Color.fromARGB(255, 255, 239, 235),
-                          ),
-                        ],
-                      ),                  
-                    ).toList(),               
-                                                   
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Observer(builder: (_) {
-                        return TextField(
-                          controller: store_chat.controllerTexto,
-                          onChanged: (value) {
-                            store_chat.setTexto(value);
-                          },
-                          decoration: InputDecoration(
-                            isDense: true,
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red, width: 2.0),
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red, width: 2.0),
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            labelText: 'Mensagem',
-                            hintText: 'Digite sua mensagem',
-                            errorText: store_chat.isValidTexto,
-                            suffixIcon: IconButton(
-                              onPressed: store_chat.save(context, email),
-                              icon: Icon(Icons.send,color: Colors.red),
-                            ),
-                          ),
-                        );
-                      }),
-                  ),
-                  SizedBox(height: 200, child: Scaffold(backgroundColor: Colors.white,)), 
-                  SizedBox(height: 50, child: Scaffold(backgroundColor: Colors.red,)), 
-                ],
-              );
-            }
-             else
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Colors.red,
-                ),
-              );
-          },
-          
+        child: Column(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+      stream: nome_map.snapshots(),
+      builder: (context, snap) {
+        if (snap.hasData) {
+          List<DocumentSnapshot> documents = snap.data!.docs;
+          documents.map(
+            (e) => nome = e['nome'],
+          ).toList();
+        }
+        return Column();
+      }
+    ),
+            
+            StreamBuilder<QuerySnapshot>(
+              // stream: FirebaseFirestore.instance.collection("mensagem").where('remetente', isEqualTo: email).where('destinatario', isEqualTo: "gustavomourago@gmail.com").orderBy('tempo').snapshots(),
+              stream: mensagens_juntas.snapshots(),
+              builder: (context, snap) {
+                if (snap.hasData) {
+                  List<DocumentSnapshot> documents = snap.data!.docs;
+                  return Column(
+                    children: [
+                      ListView(
+                        shrinkWrap: true,
+                        children:           
+                        documents.map(
+                          (e) => Column(
+                            children: [
+                              if (e['remetente'] == email)
+                                  Card(
+                                  child: Column(
+                                    children: [
+                                      ListTile(      
+                                        onTap: (){},  
+                                        hoverColor: Colors.black,              
+                                        title: Text(e['texto']),
+                                        subtitle: Text(nome),
+                                      ),        
+                                    ],
+                                  ),
+                                  color: Color.fromARGB(255, 238, 247, 255),
+                                ),
+                              if (e['destinatario'] == email)
+                                  Card(
+                                  child: Column(
+                                    children: [
+                                      ListTile(      
+                                        onTap: (){},  
+                                        hoverColor: Colors.black,              
+                                        title: Text(e['texto']),
+                                        subtitle: Text('Gustavo Moura Barros'),
+                                      ),        
+                                    ],
+                                  ),
+                                  color: Color.fromARGB(255, 255, 239, 235),
+                                ),
+                            ],
+                          ),                  
+                        ).toList(),               
+                                                       
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Observer(builder: (_) {
+                            return TextField(
+                              controller: store_chat.controllerTexto,
+                              onChanged: (value) {
+                                store_chat.setTexto(value);
+                              },
+                              decoration: InputDecoration(
+                                isDense: true,
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red, width: 2.0),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red, width: 2.0),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                labelText: 'Mensagem',
+                                hintText: 'Digite sua mensagem',
+                                errorText: store_chat.isValidTexto,
+                                suffixIcon: IconButton(
+                                  onPressed: store_chat.save(context, email),
+                                  icon: Icon(Icons.send,color: Colors.red),
+                                ),
+                              ),
+                            );
+                          }),
+                      ),
+                      SizedBox(height: 200, child: Scaffold(backgroundColor: Colors.white,)), 
+                      SizedBox(height: 50, child: Scaffold(backgroundColor: Colors.red,)), 
+                    ],
+                  );
+                }
+                 else
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                    ),
+                  );
+              },
+              
+            ),
+          ],
         ),
       ),
       
